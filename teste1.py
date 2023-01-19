dfc75984301ee919aee9c4b9e36c8102b991f947
juniobarret <juniobarret@gmail.com>
2023-01-19 09:38:45 -0300

    v1

---
+++ teste1.py
@@ -0,0 +1,12 @@
+#programa para testar senhas wifi
+
+import os
+import time
+
+#programa:
+os.system('clear')
+print('''
+    1 - Testar senha
+    2 - Sair
+''')
+opcao = input('Digite a opção desejada: 
